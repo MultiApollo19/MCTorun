387f229f82b72eeca1bfcf2d0c571a251e279865
MultiApollo19 <mariusz.belwon@gmail.com>
2026-04-20 21:19:12 +0200

    Dodaj informację o fork z CoreScope
    - Dodaj sekcję Fork Attribution w README.md z linkiem do oryginalnego projektu
    - Dodaj atrybucję w footer strony home z linkiem do https://github.com/Kpa-clawbot/CoreScope
    - Dodaj style dla .home-attribution i .home-level-toggle w home.css

--- public/home.js
+++ public/home.js
@@ -117,6 +117,11 @@
           <small>${exp ? 'Want setup guides? ' : 'Already know MeshCore? '}
           <a href="#" id="toggleLevel" style="color:var(--accent)">${exp ? 'Show new user tips' : 'Hide guides'}</a></small>
         </div>
+        <div class="home-attribution">
+          <small>
+            CoreScope fork based on <a href="https://github.com/Kpa-clawbot/CoreScope" target="_blank" rel="noopener" style="color:var(--accent)">original project</a>
+          </small>
+        </div>
       </section>
     `;
 
